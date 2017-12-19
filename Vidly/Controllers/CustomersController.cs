b15089c6271531731e7cfa352f8b2ef32ce23a3f
181192 <181192@stud.hvl.no>
2017-12-19 19:51:54 +0100

    Added name to membership type, and uploaded default values to membership type

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,22 +4,34 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -27,16 +39,6 @@ namespace Vidly.Controllers
             }
 
             return View(customer);
-        }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{ Id = 1, Name = "John Smith" },
-                new Customer{ Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
