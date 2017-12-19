0d58db499f9a150e9237ee9851090d9199eda058
181192 <181192@stud.hvl.no>
2017-12-19 23:40:38 +0100

    fetching movies from the database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,31 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-
+            _context = new ApplicationDbContext();
         }
 
+        protected override void Dispose(bool dispose)
+        {
+            _context.Dispose();
+        }
+        
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
@@ -38,18 +31,37 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
             {
-                new Movie { Name = "Shrek!" },
-                new Movie { Name = "Nemo" }
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var id = new Random().Next(1, _context.Movies.ToList().Count);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
             };
+
+            return View(viewModel);
 
         }
     }
