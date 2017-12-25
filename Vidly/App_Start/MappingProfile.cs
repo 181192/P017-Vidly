eefbbec9beaf4af770bbf57ea579bb5a0929b5c4
181192 <181192@stud.hvl.no>
2017-12-25 14:53:59 +0100

    Implemented redering movies webapi-call to DataTable

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -14,8 +14,11 @@ namespace Vidly.App_Start
         {
             // Domain to Data Transfer Object
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<Movie, MovieDto>();
             Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
 
             // Data Transfer Object to Domain
             // Property 'Id' is part of object's key information and cannot be modified
