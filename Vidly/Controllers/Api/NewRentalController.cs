438a66e3a737ea524716bbdc1cadf8316fb6951f
181192 <181192@stud.hvl.no>
2018-01-06 18:21:55 +0100

    Added Rental to domain model, and added NewRentalController

---
+++ Vidly/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            return null;
+        }
+    }
+}
